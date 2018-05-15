8b04664af2f85fc3b86b8a61da09d827fff9956f
Shane Whitehead <obishiro@gmail.com>
2018-05-15 20:00:16 +1000

    Initial Commit

---
+++ TestButtonAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestButtonAnimation
+//
+//  Created by Shane Whitehead on 15/5/18.
+//  Copyright © 2018 Shane Whitehead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
